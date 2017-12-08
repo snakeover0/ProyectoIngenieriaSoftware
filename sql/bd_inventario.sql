0ef4b16c066fe46c45e6ed15c825a8353ca14b93
snakeover0 <snakeover0@gmail.com>
2017-12-08 16:33:40 -0600

    segunda edicion --blackcode

--- sql/bd_inventario.sql
+++ sql/bd_inventario.sql
@@ -11,63 +11,68 @@ CREATE TABLE estante(
 CREATE TABLE proveedores(
 	no_proveedor SMALLINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	nombre VARCHAR(30),
-    telefono VARCHAR(10)
+    	telefono VARCHAR(10)
 );
+
 #ALTERAMOS EL INICIO DEL PK
 ALTER TABLE proveedores AUTO_INCREMENT=1000;
 
-
 CREATE TABLE usuarios(
 	usuario VARCHAR(30) NOT NULL,
-    contrasenia VARCHAR(40) NOT NULL,
-    auntenticacion BOOLEAN DEFAULT FALSE,
-    tipo VARCHAR(8) CHECK (tipo='empleado' OR tipo='admin')
+    	contrasenia VARCHAR(40) NOT NULL,
+    	auntenticacion BOOLEAN DEFAULT FALSE,
+    	tipo VARCHAR(8) CHECK (tipo='empleado' OR tipo='admin')
 );
 
 CREATE TABLE productos(
 	no_producto INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	nombre VARCHAR(30) NOT NULL,
-    descripcion TEXT,
-    cantidad SMALLINT UNSIGNED DEFAULT 0 CHECK (cantidad >= 0),	
-    tipo VARCHAR(30),	
-    sector VARCHAR(2),
-    proveedor SMALLINT,
-    FOREIGN KEY (sector) REFERENCES estante(id_sector) ON UPDATE CASCADE,
-    FOREIGN KEY (proveedor) REFERENCES proveedores(no_proveedor)
+    	descripcion TEXT,
+   	cantidad SMALLINT UNSIGNED DEFAULT 0 CHECK (cantidad >= 0),	
+    	tipo VARCHAR(30),	
+    	sector VARCHAR(2),
+    	proveedor SMALLINT,
+    	FOREIGN KEY (sector) 
+	REFERENCES estante(id_sector) 
+	ON UPDATE CASCADE,
+    	FOREIGN KEY (proveedor) 
+	REFERENCES proveedores(no_proveedor)
 );
+
 #ALTERAMOS EL INICIO DEL PK
 ALTER TABLE productos AUTO_INCREMENT=4020;
 
 CREATE TABLE producto_proveedor(
 	producto INT UNSIGNED,
-    proveedor SMALLINT,
-    FOREIGN KEY (producto) 
-		REFERENCES productos(no_producto) 
+    	proveedor SMALLINT,
+    	FOREIGN KEY (producto) 
+	REFERENCES productos(no_producto) 
         ON DELETE CASCADE,
-    FOREIGN KEY (proveedor) 
-		REFERENCES proveedores(no_proveedor) 
+    	FOREIGN KEY (proveedor) 
+	REFERENCES proveedores(no_proveedor) 
         ON DELETE CASCADE
 );
 
 
 #AQUI COMIENZAN LOS INSERT DE PRUEBA
 
-insert into estante values(
+INSERT INTO estante VALUES(
 	'a',
-    'Este estante contiene tornillos'
+    	'Este estante contiene tornillos'
 );
 
-insert into usuarios (usuario, contrasenia, tipo) values(
+INSERT INTO usuarios (usuario, contrasenia, tipo) VALUES(
 	'snakeover',
-     sha1('snake'), #PASSWORD WITH SHA1
-    'admin'
+     	sha1('snake'), #PASSWORD WITH SHA1
+    	'admin'
 );
 
-insert into proveedores (nombre, telefono) values(
+INSERT INTO proveedores (nombre, telefono) VALUES(
 	'metalcrue','9512567678'
 );
 
-insert into productos (nombre, descripcion, tipo, sector, proveedor) values(
+INSERT INTO productos (nombre, descripcion, tipo, sector, proveedor) 
+VALUES(
 	'tornillo_x',
 	'undefined',
 	'tornillos',
@@ -75,16 +80,17 @@ insert into productos (nombre, descripcion, tipo, sector, proveedor) values(
 	'1000'
 );
 
-insert into producto_proveedor values(
+INSERT INTO producto_proveedor VALUES(
 	4020,
-    1000
+    	1000
 );
 
 #AQUI COMIENZAN LOS SELECT DE PRUEBA
-select * from productos;
-select * from usuarios;
-select * from proveedores;
-select * from estante;
-select * from producto_proveedor;
+
+SELECT * FROM productos;
+SELECT * FROM usuarios;
+SELECT * FROM proveedores;
+SELECT * FROM estante;
+SELECT * FROM producto_proveedor;
 
 #***///--AQUI TERMINAN LOS PROCESOS CON LOS BASES DE DATOS--\\\***
